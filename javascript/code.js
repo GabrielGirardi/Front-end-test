75a9b1da3091e5e7ceba2e69415ba838fc3605dc
Gabriel Girardi <88117217+GabrielGirardi@users.noreply.github.com>
2022-06-30 21:59:17 -0300

    Ref: Redirecionado linhas de codigo para melhorar a organizacao

--- javascript/code.js
+++ javascript/code.js
@@ -1,10 +1,28 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
 
 
 
-// $(document).ready(function() {
-   
-// }); 
+window.onload = function() {
+    var show = document.querySelector(".arrow-down");
+    var show = document.querySelector(".more-info");
+    var arrow = document.querySelector(".arrow-down");
+
+    show.addEventListener("click", function() {
+
+    var menu = document.querySelector(".menu");
+
+    if(menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+
+    if(menu.style.display === "block") {
+        arrow.style.transform = "rotate(180deg)";
+        arrow.style.transition = "all 0.3s";
+    } else {
+        arrow.style.transform = "rotate(0deg)";
+        arrow.style.transition = "all 0.3s";
+    }
+    });
+ };
 
